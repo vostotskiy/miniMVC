ea134948bd99d4b3b5365856c8155d0493c60b34
vitalii vostotskyi <vostotskiy@mail.ru>
2017-08-13 19:18:39 +0300

    dump updated

--- dump.sql
+++ dump.sql
@@ -6,10 +6,10 @@ CREATE TABLE students
     email VARCHAR(255)  NULL,
     date_of_birth DATETIME NOT NULL,
     sex ENUM('male', 'female') NOT NULL,
-    `group` VARCHAR(255) NOT NULL,
-    faculty VARCHAR(255) NOT NULL
+    group_name VARCHAR(255) NOT NULL,
+    faculty_name VARCHAR(255) NOT NULL
 );
-INSERT INTO mini_mvc.students (first_name, last_name, email, date_of_birth, sex, `group`, faculty) VALUES
+INSERT INTO mini_mvc.students (first_name, last_name, email, date_of_birth, sex, group_name, faculty_name) VALUES
 ('John', 'Doe', 'doe@gmail.com' , '2008-07-04 00:00:00', 'male', 'XZ-1', 'Gryffindor'),
 ('Super', 'Woman', 'your_super_woman@gmail.com', '2009-08-05 00:00:00', 'female', 'YY-2', 'Ravenclaw'),
 ('Extra', 'Person', 'something@extra.com', '2009-08-05 00:00:00', 'female', 'XY-2', 'Slytherin');

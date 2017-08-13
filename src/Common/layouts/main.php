857ad7780dad72ebb0eae79c9c0759d4c2a2924a
vitalii vostotskyi <vostotskiy@mail.ru>
2017-08-13 17:05:59 +0300

    main template fixes, added links

--- src/Common/layouts/main.php
+++ src/Common/layouts/main.php
@@ -10,18 +10,18 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Logo</a>
+            <a class="navbar-brand" href="/">miniMVC Logo</a>
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
+                <li class="active"><a href="/">Home</a></li>
                 <li><a href="#">About</a></li>
-                <li><a href="#">Projects</a></li>
-                <li><a href="#">Contact</a></li>
+<!--                <li><a href="#">Projects</a></li>-->
+<!--                <li><a href="#">Contact</a></li>-->
             </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-            </ul>
+<!--            <ul class="nav navbar-nav navbar-right">-->
+<!--                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>-->
+<!--            </ul>-->
         </div>
     </div>
 </nav>
--- src/Students/views/index/index.php
+++ src/Students/views/index/index.php
@@ -1,7 +1,6 @@
 <div class="container">
     <div class="row">
 
-
         <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-heading"><h4>Students</h4></div>
